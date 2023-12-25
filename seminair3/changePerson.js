e0bb2a3021396c15bb64a27614d3aacd1daeff44
Vladimir <maostyle@mail.ru>
2023-12-25 18:36:11 +0300

    seminair 3

---
+++ seminair3/changePerson.js
@@ -0,0 +1,20 @@
+// Создайте файл changePerson.js рядом с файлом writePerson.js
+// 2. Напишите в нем код, который➜ прочитает файл person.json, ➜уменьшит возраст на 10➜ изменит город на“ Ekaterinburg”➜ перезапишет исходный файл person.json
+
+const fs = require('fs');
+const path = require('path');
+const pathToFile = path.join(__dirname, 'person.json');
+
+const user = {
+    name: "Ivan",
+    surname: "Ivanov",
+    age: "30",
+    city: "Moscow"
+}
+
+const data = JSON.parse(fs.readFileSync(pathToFile, 'utf-8'));
+
+data.age -= 10;
+data.city = "Ekaterinburg";
+
+fs.writeFileSync(pathToFile, JSON.stringify(data, null, 2));
